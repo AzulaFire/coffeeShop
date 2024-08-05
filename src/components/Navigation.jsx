9184f8747276e6e8c91e024921bc565e865b805a
AzulaFire <jhornjr@gmail.com>
2024-08-05 17:38:56 +0900

    fixed header and hero spacing

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,27 +2,27 @@ import { SiCoffeescript } from 'react-icons/si';
 
 const Navigation = () => {
   return (
-    <div className='flex flex-row justify-center bg-[#016241] h-[70px] sticky top-0 z-10 border-b-[#131313] border-b-2 shadow-2xl'>
-      <div className='gap-3 m-3 text-3xl text-white my-[20px] pr-4'>
+    <div className='flex flex-row justify-center bg-[#016241] h-[70px] sticky top-0 z-10 border-b-[#131313] border-b-2 shadow-2xl px-2'>
+      <div className='gap-3 m-3 text-3xl text-white my-[20px] pr-2'>
         <SiCoffeescript />
       </div>
       <div className='flex'>
         <a href='#Hero'>
-          <div className='gap-3 m-3 text-2xl text-white my-[20px] pr-4 font-yanone'>
+          <div className='gap-3 m-3 text-2xl text-white my-[20px] pr-2 font-yanone'>
             Home
           </div>
         </a>
       </div>
       <div className='flex'>
         <a href='#About'>
-          <div className='gap-3 m-3 text-2xl text-white my-[20px] pr-4 font-yanone'>
+          <div className='gap-3 m-3 text-2xl text-white my-[20px] pr-2 font-yanone'>
             About
           </div>
         </a>
       </div>
       <div className='flex'>
         <a href='#Services'>
-          <div className='gap-3 m-3 text-2xl text-white my-[20px] pr-4 font-yanone'>
+          <div className='gap-3 m-3 text-2xl text-white my-[20px] pr-2 font-yanone'>
             Services
           </div>
         </a>
