ae2d95f243ec71b464308fade841a56b1a9b6b02
AzulaFire <jhornjr@gmail.com>
2024-08-07 15:24:23 +0900

    reversed cofffee form image requirments

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -144,6 +144,7 @@ const AddCoffee = () => {
                 type='text'
                 id='image'
                 name='image'
+                required
                 value={form.image}
                 onChange={handleChange}
                 className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500 sm:text-sm'
